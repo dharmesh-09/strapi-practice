d63efdcf17878a3415f7f8467cda78462e8383ff
dharmesh-09 <dharmesh.t@nighthack.in>
2025-01-29 12:22:58 +0530

    modify database.js

--- config/database.js
+++ config/database.js
@@ -1,24 +1,17 @@
 const path = require('path');
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'mysql');  // Set default to 'mysql'
 
   const connections = {
     mysql: {
       connection: {
-        host: env('DATABASE_HOST', 'localhost'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        host: env('DATABASE_HOST', 'mysql.railway.app'),  // Update with your Railway MySQL host
+        port: env.int('DATABASE_PORT', 3306),  // Default MySQL port
+        database: env('DATABASE_NAME', 'railway'),  // Use your actual database name
+        user: env('DATABASE_USERNAME', 'root'),  // Default MySQL username
+        password: env('DATABASE_PASSWORD', 'GWPMIOsyrUiVkeIpdhMkKXbXxdpkzSpN'),  // Your password from Railway environment
+        ssl: env.bool('DATABASE_SSL', false),  // Set SSL to false unless needed
       },
       pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
     },
