cf1b109678b05b78e3eddc1615a6065dba81d131
dharmesh-09 <dharmesh.t@nighthack.in>
2025-01-29 12:51:02 +0530

    add plugin.js file jwt secret

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,8 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    'users-permissions': {
+      config: {
+        jwtSecret: env('JWT_SECRET', 'ozd0mX3nDJ5xy14hI1WSePgQb0zZIK1YOrCk9vQ7Rkc=='), // Use the generated JWT_SECRET here
+      },
+    },
+  });
+  
