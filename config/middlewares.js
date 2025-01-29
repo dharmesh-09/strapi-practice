362781c4408dd2b714dcd395c16195696d4cca3e
dharmesh-09 <dharmesh.t@nighthack.in>
2025-01-29 13:20:38 +0530

    middleware.js file updated for cors errors

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Allow all origins or replace with 'https://astonishing-kringle-8619e7.netlify.app' to allow only your frontend
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+      allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+      credentials: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
