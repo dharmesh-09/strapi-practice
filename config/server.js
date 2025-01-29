d4c4342716cd67bb24b267e81f4ee0a343ec769c
dharmesh-09 <dharmesh.t@nighthack.in>
2025-01-29 12:45:28 +0530

    add keys in server.js file

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: ['VWNqXaC20CgQQzyZ2lsSr6CIbC+5ItQ+4YcWxn3uJ18=', '2xSru/MLa41ElUVcsCHsG1qDZlNYHMRQ0mRVqlT3IJU='],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
