dcfc2a409ac789a68f7d50a0a2c7a004877a2a1d
dharmesh-09 <dharmesh.t@nighthack.in>
2025-01-29 14:29:21 +0530

    refacotoring

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,7 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-        jwtSecret: env('JWT_SECRET', 'xT5hfo1hG62LVYp9w2ZsZA=='), // Use the generated JWT_SECRET here
-      },
+  'users-permissions': {
+    config: {
+      jwtSecret: 'xT5hfo1hG62LVYp9w2ZsZA==', // Use the generated JWT_SECRET here
     },
-  });
-  
+  },
+});
--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: '8080',
   app: {
-    keys: ['VWNqXaC20CgQQzyZ2lsSr6CIbC+5ItQ+4YcWxn3uJ18=', '2xSru/MLa41ElUVcsCHsG1qDZlNYHMRQ0mRVqlT3IJU='],
+    keys: ['VWNqXaC20CgQQzyZ2lsSr6CIbC+5ItQ+4YcWxn3uJ18=', '2xSru/MLa41ElUVcsCHsG1qDZlNYHMRQ0mRVqlT3IJU=keys'],
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
